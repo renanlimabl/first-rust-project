5114d5400a79efb1b34799920620de5895342019
Renan Lima <cursosprogramacoes@gmail.com>
2021-05-16 03:32:17 -0300

    first rust project

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use ferris_says::say; // This import say of the ferris_says
+
+use std::io::{stdout, BufWriter};
+
+fn main() {
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
+}
